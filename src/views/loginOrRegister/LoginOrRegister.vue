e4e90b584715416cf7e777ceae784d5424634a65
Juan Salazar <juan.salazar@grupocsi.com>
2022-03-29 04:14:13 -0600

    Added login form

--- src/views/loginOrRegister/LoginOrRegister.vue
+++ src/views/loginOrRegister/LoginOrRegister.vue
@@ -3,7 +3,7 @@
     fluid
     v-layout="'public-layout'"
     class="fill-height pa-0 back"
-    :class="{ 'sign-up-active': signIn }"
+    :class="{ 'sign-in-active': signIn }"
   >
     <div class="overlay-container">
       <div class="overlay">
@@ -17,7 +17,7 @@
             color="transparent"
           >
             <v-card-title class="justify-center">POSTULARSE</v-card-title>
-            <v-card-text class="text--white">
+            <v-card-text class="text&#45;&#45;white">
               <div>Lorem ipsum dolor sit amet.</div>
               <div>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -48,7 +48,7 @@
             color="transparent"
           >
             <v-card-title class="justify-center">INICIAR SESIÓN</v-card-title>
-            <v-card-text class="text--white">
+            <v-card-text class="text&#45;&#45;white">
               <div>Lorem ipsum dolor sit amet.</div>
               <div>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -71,13 +71,28 @@
         </div>
       </div>
     </div>
+    <v-row>
+      <v-col cols="6">
+        <div class="d-flex align-center justify-center">
+          <h1>Formulario de postulantes</h1>
+        </div>
+      </v-col>
+      <v-col cols="6">
+        <div class="d-flex align-center justify-center">
+          <sign-in-form />
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import SignInForm from "@/views/loginOrRegister/partials/signIn/SignInForm.vue";
 
-@Component({})
+@Component({
+  components: { SignInForm },
+})
 export default class LoginOrRegister extends Vue {
   public signIn = false;
 }
