37591d731b1f3235962e86dda189dfae59b6b87b
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-04-28 20:17:35 -0500

    Fixed sidebar translation change

--- src/store/app/index.ts
+++ src/store/app/index.ts
@@ -1,15 +1,11 @@
 import { IAppState } from "@/store/app/types";
 import { Module } from "vuex";
 import { IRootState } from "@/store/types";
-import appMenu from "@/components/layouts/partials/menu";
 import i18n from "@/lang";
 
 const initialState: IAppState = {
   layout: "public-layout",
   lang: i18n.locale,
-  sidebar: {
-    menu: appMenu,
-  },
 };
 
 const appStore: Module<IAppState, IRootState> = {
