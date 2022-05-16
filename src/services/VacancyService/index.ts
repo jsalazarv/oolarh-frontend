770946d747018d16c52d38ad01bb3ee8f8bcc38b
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-05-16 14:03:02 -0500

    Added functionality to edit vacancies

--- src/services/VacancyService/index.ts
+++ src/services/VacancyService/index.ts
@@ -15,8 +15,16 @@ export default class VacancyService extends BaseService {
     return this.client.get("/vacancies", query);
   }
 
+  findById(id: string, params = {}): IPlainResponse<IVacancy> {
+    return this.client.get(`/vacancies/${id}/`, params);
+  }
+
   create(vacancy: IVacancyRequest): IPlainResponse<IVacancy> {
     return this.client.post(`/vacancies`, vacancy);
+  }
+
+  update(department: IVacancyRequest): IPlainResponse<IVacancy> {
+    return this.client.put(`/vacancies/${department.id}`, department);
   }
 
   delete(id: number): INoContentResponse {
--- src/services/VacancyService/types.ts
+++ src/services/VacancyService/types.ts
@@ -1,24 +1,26 @@
 import { IJob } from "@/services/JobService/types";
 import { IQueryParams } from "@/services/types";
+import { IBranchOffices } from "@/services/BranchOfficesService/types";
+import { IDepartment } from "@/services/DepartmentService/types";
 
 export interface IVacancy {
   id: number | null;
   name: string;
   description: string;
   salary: string;
-  branch_office: number | null;
-  department: number | null;
+  branch_office: IBranchOffices;
+  department: IDepartment;
   job: IJob;
 }
 
 export interface IVacancyRequest {
-  id: number | null;
-  name: string;
-  description: string;
-  salary: string;
-  branch_office_id: number | null;
-  department_id: number | null;
-  job_id: number | null;
+  id?: number | null;
+  name?: string;
+  description?: string;
+  salary?: string;
+  branch_office_id?: number | null;
+  department_id?: number | null;
+  job_id?: number | null;
 }
 
 export type IVacanciesQueryParams = IQueryParams;
