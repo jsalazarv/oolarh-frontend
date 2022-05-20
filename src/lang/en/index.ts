d28f7ed5e4699b26e6aefa59502d1c19b451242b
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-05-20 16:01:43 -0500

    Added translations in branches section

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -1,11 +1,13 @@
 import dictionary from "./dictionary";
 import sidebar from "@/lang/en/sidebar";
+import branchOffices from "@/lang/en/branchOffices";
 import departments from "@/lang/en/departments";
 import jobs from "@/lang/en/jobs";
 
 export default {
   dictionary,
   sidebar,
+  branchOffices,
   departments,
   jobs,
 };
