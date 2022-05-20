985682e3ad5d0989056148f1daa4e4a3c2137b8e
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-05-20 16:24:00 -0500

    Added translations for applicants module

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -1,5 +1,6 @@
 import dictionary from "./dictionary";
 import sidebar from "@/lang/en/sidebar";
+import applications from "@/lang/en/applications";
 import vacancies from "@/lang/en/vacancies";
 import branchOffices from "@/lang/en/branchOffices";
 import departments from "@/lang/en/departments";
@@ -8,6 +9,7 @@ import jobs from "@/lang/en/jobs";
 export default {
   dictionary,
   sidebar,
+  applications,
   vacancies,
   branchOffices,
   departments,
