29b33806429e17a1d6281340bb423826d3fe36f3
Juan Salazar <jsalazarv@MacBook-Pro-de-Juan.local>
2022-07-26 00:27:21 -0500

    Added toggle format

--- src/views/jobs/components/ResponseDialog.vue
+++ src/views/jobs/components/ResponseDialog.vue
@@ -12,7 +12,9 @@
       <v-card-text class="py-0">
         <prism :plugins="['line-numbers']" :code="textCode"></prism>
       </v-card-text>
-      <v-card-actions> </v-card-actions>
+      <v-card-actions>
+        <v-btn @click="changeFormat">{{ inverseFormat }}</v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -21,6 +23,9 @@
 import { Component, PropSync, Vue, Watch } from "vue-property-decorator";
 import Prism from "vue-prismjs";
 import "prismjs/themes/prism.css";
+import * as js2xmlparser from "js2xmlparser";
+
+type Formats = "json" | "xml";
 
 @Component({
   components: { Prism },
@@ -32,17 +37,29 @@ export default class ResponseDialog extends Vue {
   data!: [];
 
   public code = [];
+  public format: Formats = "json";
 
   @Watch("response")
   setData(): void {
     if (this.isDialogOpen) {
       this.code = this.data;
-      console.log("code", this.code);
     }
   }
 
+  changeFormat(): void {
+    this.format = this.inverseFormat;
+  }
+
+  get inverseFormat(): Formats {
+    return this.format === "xml" ? "json" : "xml";
+  }
+
   get textCode(): string {
-    return JSON.stringify(this.code, null, "\t");
+    if (this.format === "json") {
+      return JSON.stringify(this.code, null, "\t");
+    }
+
+    return js2xmlparser.parse("text", this.code);
   }
 
   closeDialog(): void {
